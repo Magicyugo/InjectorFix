f2d150f1a8441550c42d888ed9843d9e4bf63457
Magicyugo <74493761+Magicyugo@users.noreply.github.com>
2024-08-29 13:11:35 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 function toggleMenu() {
-    console.log("Hamburger menu clicked"); // Debugging-Zweck
+    console.log("Hamburger menu clicked"); // Debugging
     const menu = document.getElementById('sideMenu');
     const hamburger = document.querySelector('.hamburger-menu');
+
+    // Füge ein Debugging-Log hinzu, um den aktuellen Display-Status zu überprüfen
+    console.log("Menu display status before toggle:", menu.style.display);
+
     if (menu.style.display === 'block') {
         menu.style.display = 'none';
         hamburger.classList.remove('open');
@@ -9,27 +13,7 @@ function toggleMenu() {
         menu.style.display = 'block';
         hamburger.classList.add('open');
     }
+
+    // Überprüfe nach dem Umschalten den Status erneut
+    console.log("Menu display status after toggle:", menu.style.display);
 }
-
-function closeMenu() {
-    const menu = document.getElementById('sideMenu');
-    const hamburger = document.querySelector('.hamburger-menu');
-    menu.style.display = 'none';
-    hamburger.classList.remove('open');
-}
-
-// Event listener für das Schließen des Menüs auf Mobilgeräten
-document.querySelectorAll('#sideMenu ul li a').forEach(link => {
-    link.addEventListener('click', () => {
-        closeMenu();
-    });
-});
-
-// Event listener für das Schließen des Menüs durch Klicken auf das X
-document.querySelector('.hamburger-menu').addEventListener('click', (e) => {
-    if (e.target.closest('.hamburger-menu').classList.contains('open')) {
-        closeMenu();
-    } else {
-        toggleMenu();
-    }
-});
