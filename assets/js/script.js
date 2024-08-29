755cb1335508e8d854056fd21d587532952a2b50
Magicyugo <74493761+Magicyugo@users.noreply.github.com>
2024-08-29 14:59:10 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const hamburger = document.querySelector('.hamburger-menu');
-    const menu = document.getElementById('sideMenu');
 
-    // Funktion zum Umschalten des Menüs
-    function toggleMenu() {
-        if (menu.style.display === 'none' || menu.style.display === '') {
-            menu.style.display = 'block';
-        } else {
-            menu.style.display = 'none';
-        }
-    }
-
-    // Hamburger-Klick-Ereignis
-    hamburger.addEventListener('click', toggleMenu);
-});
