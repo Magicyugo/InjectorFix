91ab5ea70c5e5c0c2442814b8b399267e6db8881
Magicyugo <74493761+Magicyugo@users.noreply.github.com>
2024-08-29 14:12:41 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,25 @@
-// Funktion zum Umschalten des Hamburger-Menüs
-function toggleMenu() {
-    const menu = document.getElementById('sideMenu');
-    if (menu.style.display === 'block') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'block';
+document.addEventListener('DOMContentLoaded', function () {
+    // Funktion zum Umschalten des Hamburger-Menüs
+    function toggleMenu() {
+        const menu = document.getElementById('sideMenu');
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';
+        } else {
+            menu.style.display = 'block';
+        }
     }
-}
 
-// Funktion zum Schließen des Hamburger-Menüs
-function closeMenu() {
-    const menu = document.getElementById('sideMenu');
-    menu.style.display = 'none';
-}
+    // Funktion zum Schließen des Hamburger-Menüs
+    function closeMenu() {
+        const menu = document.getElementById('sideMenu');
+        menu.style.display = 'none';
+    }
 
-// Event Listener für das Schließen des Menüs, wenn eine Option ausgewählt wird
-document.querySelectorAll('#sideMenu ul li a').forEach(item => {
-    item.addEventListener('click', closeMenu);
+    // Event Listener für das Öffnen und Schließen des Hamburger-Menüs
+    document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
+
+    // Event Listener für das Schließen des Menüs, wenn eine Option ausgewählt wird
+    document.querySelectorAll('#sideMenu ul li a').forEach(item => {
+        item.addEventListener('click', closeMenu);
+    });
 });
-
-// Event Listener für das Öffnen und Schließen des Hamburger-Menüs
-document.querySelector('.hamburger-menu').addEventListener('click', toggleMenu);
