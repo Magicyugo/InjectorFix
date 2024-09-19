c22f64938bc55b78938084eecb73ea9bfb6ee2a6
Magicyugo <74493761+Magicyugo@users.noreply.github.com>
2024-09-19 10:17:42 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
+function toggleMenu() {
+    const menu = document.getElementById('sideMenu');
+    const hamburger = document.querySelector('.hamburger-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+        hamburger.classList.remove('open');
+    } else {
+        menu.style.display = 'block';
+        hamburger.classList.add('open');
 
+        // Setze die Position des Menüs relativ zur Hamburger-Position
+        const hamburgerOffset = hamburger.getBoundingClientRect();
+        menu.style.top = hamburgerOffset.bottom + 'px'; // Immer auf der Höhe des Symbols aufklappen
+    }
+}
